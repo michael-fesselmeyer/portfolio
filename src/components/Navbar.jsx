4f04bbe727071beb958c92fd83e0671de1175f0c
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-24 14:11:55 -0400

    Added icons to navbar w/ links for github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import { FaGithub, FaEnvelope, FaLinkedin } from 'react-icons/fa';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <>
     <div className="navbar">
-    <div className="navbar-icons">Icons</div>
-    <div className="navbar-links">Links</div>
+      <div className="navbar-icons">
+        <a href="https://github.com/michael-fesselmeyer" target="_blank" rel="noreferrer">
+          <FaGithub />
+        </a>
+        <a href="mailto:michael.fesselmeyer@gmail.com">
+          <FaEnvelope />
+        </a>
+        <a href="https://www.linkedin.com/in/michael-fesselmeyer/" target="_blank" rel="noreferrer">
+          <FaLinkedin />
+        </a>
+      </div>
+      <div className="navbar-links">Links</div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
