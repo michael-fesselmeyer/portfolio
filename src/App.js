2d3d3b6fd24f4bd6c85b77c89ecd0ad0fd5667ee
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-24 13:16:33 -0400

    Started website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App-header">
+        <Navbar />
+      </div>
+      <div className="App-body">
+        <About />
+        <Contact />
+        <Portfolio />
+        <Skills />
+      </div>
+      <div className="App-footer">
+        <Footer />
+      </div>
+    </>
   );
 }
 
