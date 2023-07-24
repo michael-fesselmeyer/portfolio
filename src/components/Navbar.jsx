644aa744920252b5bff2e10e77a8b14e603816ba
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-24 13:28:07 -0400

    Basic stuff on the page, needs style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,10 @@ import './Navbar.css'
 function Navbar() {
   return (
     <>
+    <div className="navbar">
     <div className="navbar-icons">Icons</div>
     <div className="navbar-links">Links</div>
+    </div>
     </>
   )
 }
