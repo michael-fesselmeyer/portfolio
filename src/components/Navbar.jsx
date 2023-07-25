f183cdf820ddf7bf8918f3d6ef2bef85f71a565f
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-25 13:53:05 -0400

    Saving status before trying particles package

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className="navbar">
+    <div className="navbar App"> {/* Add "App" class here */}
       <div className="navbar-icons">
         <a href="https://github.com/michael-fesselmeyer" target="_blank" rel="noreferrer">
-          <FaGithub />
+          <FaGithub className="github-icon-navbar" /> {/* Add a specific class for GitHub icon */}
         </a>
         <a href="mailto:michael.fesselmeyer@gmail.com">
-          <FaEnvelope />
+          <FaEnvelope className="envelope-icon" />
         </a>
         <a href="https://www.linkedin.com/in/michael-fesselmeyer/" target="_blank" rel="noreferrer">
-          <FaLinkedin />
+          <FaLinkedin className="linkedin-icon" />
         </a>
         <a href="https://docs.google.com/document/d/1kN63pkJlczyeRlNiB_-dk0_OKvhv2BB9YDiIZKF5eig/edit" target="_blank" rel="noreferrer">
-          <FaFileDownload />
+          <FaFileDownload className="filedownload-icon" />
         </a>
       </div>
       <div className="navbar-links">
